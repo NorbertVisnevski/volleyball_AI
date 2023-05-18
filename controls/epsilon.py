699730c3881b64c3288e3f7e8b1becca23006ea7
Norbert Visnevski <norbert.visnevski@gmail.com>
2023-05-19 01:25:19 +0300

    finalizations

--- controls/epsilon.py
+++ controls/epsilon.py
@@ -8,3 +8,17 @@ class EpsilonGreedy:
 
     def reset(self):
         self.epsilon = 1
+
+class EpsilonGreedyDecreasing:
+    def __init__(self, decay_rate=0.01, global_decay=0.01):
+        self.epsilon = 1
+        self.reset_epsilon = 1
+        self.decay_rate = 1-decay_rate
+        self.global_decay = 1-global_decay
+
+    def decay(self):
+        self.epsilon *= self.decay_rate
+
+    def reset(self):
+        self.reset_epsilon *= self.global_decay
+        self.epsilon = self.reset_epsilon
